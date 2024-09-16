817ea9b95952b70152ad65335fd25cd417588e96
Sreeramofficial <mssreerammanatt@gmail.com>
2024-09-16 18:09:45 +0530

    initial commit

---
+++ src/main/java/com/saturnPulse/SaturnPulse/SaturnPulseApplication.java
@@ -0,0 +1,13 @@
+package com.saturnPulse.SaturnPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaturnPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaturnPulseApplication.class, args);
+	}
+
+}
